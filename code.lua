72e67f0e91f317818949a2a3319462ad8c4dded0
MavricMC <118589100+MavricMC@users.noreply.github.com>
2023-11-05 15:50:21 +1300

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,3 +1,35 @@
---Im amazing at coding
+//How to return a value
+//{"to":"turtle0", "type":"return", "message":"return  turtle.getFuelLevel()"}
+local ws, err = http.websocket("ws://localhost:3000")
+os.loadAPI("/json")
 
---Yes its true
+if err then
+    error(err)
+elseif ws then
+    while true do
+        local m = ws.receive()
+        term.clear()
+        term.setCursorPos(1, 1)
+        if m == "" then
+            m = "{}"
+        end
+        print(m)
+        local msg = json.decode(m)
+        printError(msg.to)
+        if msg.to ~= "turtle0" then
+            print("Not for me")
+        else
+            print(msg.type, ":", msg.message)
+            if msg.type == "code" or msg.type == "return" then
+                local func = load(msg.message)
+                local ret = func()
+                if msg.type == "return" then
+                    retur = {to = "website", type = "webreturn", message = tostring(ret)}
+                    local returnMsg = json.encode(retur)
+                    ws.send(returnMsg)
+                    printError("Return msg:", returnMsg)
+                end
+            end
+        end
+    end
+end
